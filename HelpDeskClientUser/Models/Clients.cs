efe1bdbdff8beac470d2bb61818a53635d8f9b9a
DavDavUdav <stepan05az@gmail.com>
2023-05-24 13:15:58 +0300

    Revert "Revert "Добавьте файлы проекта.""
    This reverts commit 994f38e3f9e4794a89181e1258591875cc6ccce4.

---
+++ HelpDeskClientUser/Models/Clients.cs
@@ -0,0 +1,41 @@
+﻿using ServerAppHelpDesk.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientAppHelpDesk.DataBase.Models
+{
+    public class Clients : IEntity
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Имя клиента.
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Фамилия клиента.
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Адрес эл. почты клиента.
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Номер телефона клиента.
+        /// </summary>
+        public string Phone { get; set; }
+        /// <summary>
+        /// Дата регистрации клиента.
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+        /// <summary>
+        /// Дата последней активности клиента.
+        /// </summary>
+        public DateTime LastActivityDate { get; set; }
+
+        public ICollection<Tickets> Tickets { get; set; }
+    }
+}
